dd9fcd10b251f08ee5a7e47a7d60c6d5b652fd86
unknown <mahendra.unhale@gmail.com>
2022-12-16 00:43:05 +0530

    second commit

---
+++ src/app/contact.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Contact } from './contact';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  private baseUrl='http://localhost:8081';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllContacts():Observable<Contact[]>{
+    return this.httpClient.get<Contact[]>(`${this.baseUrl}/contacts`);
+  }
+  createContact(contact:Contact):Observable<Object>{
+    return this.httpClient.post(`${this.baseUrl}/contact`,contact,{responseType:"text"});
+  }
+  removeContact(id:number):Observable<Object>{
+    return this.httpClient.delete(`${this.baseUrl}/contact/${id}`,{responseType:"text"})
+  }
+  findContact(id:number):Observable<Contact>{
+    return this.httpClient.get<Contact>(`${this.baseUrl}/contact/${id}`)
+  }
+
+  updateContactDetils(contact:Contact):Observable<string>{
+    return this.httpClient.put(`${this.baseUrl}/contact`, contact, {
+      responseType: 'text',
+    });
+  }
+  
+}
